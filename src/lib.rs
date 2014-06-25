55159e7e677c16807784c5a9c2f2239cc6dd40c6
theptrk <patrick.tran06@gmail.com>
2014-06-25 11:11:59 -0700

    (doc) Made for readable

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 //! Url Encoded middleware for Iron
 //! 
 //! This middleware focuses on parsing the incoming url parameters from client requests.
-//! It creates a HashMap that maps String to a Vec of Strings to account for 
-//! clients that pass more than one value to a given key. 
 #![crate_id = "urlencoded"]
 #![license = "MIT"]
 
@@ -17,20 +15,18 @@ use iron::middleware::{Status, Continue, Unwind};
 use url::from_str;
 use std::collections::HashMap;
 
-/// Stores a hashmap of a string and a Vec of strings to address
-/// client data that is sent with multiple values for a single key. This
-/// structure allows for access to all values passed without overwriting
-/// previous values assigned to the same key.
+/// Stores a `HashMap` of a `String` and a `Vec<Strings>` to address
+/// client data that is sent with multiple values for a single key.
 #[deriving(Clone)]
 pub struct Encoded(pub HashMap<String, Vec<String>>);
 
 /// This middleware is used for parsing url parameters and storing
-/// the data as conveniently accessable data `insert`ed into an Alloy. 
+/// the data as conveniently accessible data `insert`ed into an Alloy. 
 #[deriving(Clone)]
 pub struct UrlEncoded;
 
-/// Creates a UrlEncoded instance to be `link`ed to `server.chain`. Calling the
-/// function will `insert` a new hashmap into the `alloy`.
+/// Creates a `UrlEncoded` instance to `link` to `server.chain`. Calling the
+/// function will `insert` a new `HashMap` into the `alloy`.
 impl UrlEncoded {
     pub fn new() -> UrlEncoded {
         UrlEncoded
